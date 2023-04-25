f9250c539bebc0f34bcd9a7c9c8f3b7c5ce37c44
yepShinjo <backguy05@gmail.com>
2023-04-25 14:38:59 +0800

    4th commit

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+const app = require('./App.jsx');
 
-ReactDOM.render(<App />, document.getElementById('root'));
+module.exports = app;
